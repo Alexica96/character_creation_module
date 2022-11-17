3e8ca1f139bedb4f6a21123892ef5eabd394178a
Alexandr Lifanov <a.lifanow@yandex.ru>
2022-11-17 18:43:44 +0300

    code according to PEP8 - practice 3g-9c-4t-p2 fix multipple mistakes

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def start_training(char_name: str, char_class: str) -> str:
           'defence — чтобы блокировать атаку противника '
           'или special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = None
+    cmd: str = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -67,8 +67,8 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class():
-    approve_choice = None
-    char_class = None
+    approve_choice: str = None
+    char_class: str = None
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого '
                            'хочешь играть: Воитель — warrior, Маг — '
@@ -82,9 +82,9 @@ def choice_char_class():
         if char_class == 'healer':
             print('Лекарь — могущественный заклинатель. '
                   'Черпает силы из природы, веры и духов.')
-        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
-                               'или любую другую кнопку, чтобы выбрать '
-                               'другого персонажа ').lower()
+        approve_choice: str = input('Нажми (Y), чтобы подтвердить выбор, '
+                                    'или любую другую кнопку, чтобы выбрать '
+                                    'другого персонажа ').lower()
     return char_class
 
 
